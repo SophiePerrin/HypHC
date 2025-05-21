91fb849ff7a48728981736c9e411a922231c9111
Sophie PERRIN <sophieperrin@MacBook-Air-de-Sophie.local>
2025-05-21 14:34:18 +0200

    transformation du set_env.sh en une version adaptée pour miniconda (au lieu de la déf d'environnement local simple) : changement de la ligne concernée ds le prog bash

--- set_env.sh
+++ set_env.sh
@@ -3,11 +3,13 @@
 # Récupère le chemin absolu du dossier contenant ce script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Active l'environnement virtuel
-source "$SCRIPT_DIR/hyphc_env/bin/activate"
+# Active l'environnement conda global
+source ~/miniconda3/bin/activate hyphc_env
+
 
 # Définit les chemins relatifs à ce dossier
 export HHC_HOME="$SCRIPT_DIR"
 export DATAPATH="$HHC_HOME/data"                # Path where to save the data files
 export SAVEPATH="$HHC_HOME/embeddings"          # Path where to save the trained models 
+
 
