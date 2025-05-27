058d61b6b2d76215e17d7174b7a58398956b522d
sophieperrinlyon2 <sophie.perrin1@univ-lyon2.fr>
2025-05-27 16:13:03 +0000

    division du programme en cellules notebook pour travail dessus (à enlever pour version définitive)

--- pick_up_embeddings.py
+++ pick_up_embeddings.py
@@ -1,17 +1,22 @@
 """Script to visualize the HypHC clustering."""
 
+# %%
 import argparse
 import json
 import os
-
-import matplotlib.pyplot as plt
 import torch
-
 from datasets.loading import load_data
 from model.hyphc import HypHC
 from utils.poincare import project
-from utils.visualization import plot_tree_from_leaves
 
+# %%
+# SERA A SUPPRIMER DU PROGRAMME DEFINITIF
+import sys
+sys.argv = ['main.py', '--model_dir', './checkpoints/exp1', '--epochs', '100']
+
+
+
+# %%
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Hyperbolic Hierarchical Clustering.")
     parser.add_argument("--model_dir", type=str, required=True,
@@ -32,11 +37,17 @@ if __name__ == "__main__":
     model.load_state_dict(params, strict=False)
     model.eval()
 
+# %%
     # decode tree
     tree = model.decode_tree(fast_decoding=True)
+# %%
     leaves_embeddings = model.normalize_embeddings(model.embeddings.weight.data)
+# %%
     leaves_embeddings = project(leaves_embeddings).detach().cpu().numpy()
-    # fig = plt.figure(figsize=(5, 5))
-    # ax = fig.add_subplot(111)
-    # ax = plot_tree_from_leaves(ax, tree, leaves_embeddings, labels=y_true)
-    # fig.savefig(os.path.join(args.model_dir, f"embeddings_{args.seed}.png"))
+# %%
+    np.save("leaves_emb.npy",leaves_embeddings)
+
+# %%
+
+    print(leaves_embeddings)
+# %%
