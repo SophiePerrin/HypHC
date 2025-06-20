7eee78959b3d402cea687809e784e00d59bfd839
sophieperrinlyon2 <sophie.perrin1@univ-lyon2.fr>
2025-06-20 13:21:45 +0000

    bonne version de set_envS3.sh

--- set_envS3.sh
+++ set_envS3.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ~/miniconda3/bin/activate hyphc_env
 
 # Définir les variables d’environnement S3
-export AWS_S3_ENDPOINT=s3.minio.lab.sspcloud.fr
+export AWS_S3_ENDPOINT=minio.lab.sspcloud.fr
 export AWS_DEFAULT_REGION=us-east-1
 
 # Définir le chemin local pour les sauvegardes
