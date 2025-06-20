77d259e8613704fe2ebb0f310f5119dd0b7146f3
sophieperrinlyon2 <sophie.perrin1@univ-lyon2.fr>
2025-06-20 10:37:02 +0000

    creation d'un fichier set_envS3.sh

--- set_env.sh
+++ set_env.sh
@@ -7,9 +7,3 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ~/miniconda3/bin/activate hyphc_env
 
 
-# Définit les chemins relatifs à ce dossier
-export HHC_HOME="$SCRIPT_DIR"
-export DATAPATH="$HHC_HOME/data"                # Path where to save the data files
-export SAVEPATH="$HHC_HOME/embeddings"          # Path where to save the trained models 
-
-
---
+++ set_envS3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Récupère le chemin absolu du dossier contenant ce script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Active l'environnement conda
+source ~/miniconda3/bin/activate hyphc_env
+
+# Définir les variables d’environnement S3
+export AWS_S3_ENDPOINT=s3.us-east-1.minio.lab.sspcloud.fr
+export AWS_DEFAULT_REGION=us-east-1
+
+# Définir le chemin local pour les sauvegardes
+export SAVEPATH=$HOME/work/HypHC/results
+
+# (Optionnel) Créer le dossier s’il n’existe pas
+# mkdir -p "$SAVEPATH"
