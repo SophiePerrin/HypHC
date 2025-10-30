e98262512affe0b5d8b9ab5fe643435dd5ace535
sophieperrinlyon2 <sophie.perrin1@univ-lyon2.fr>
2025-10-30 00:42:34 +0000

    retablissement de ma version du fork

--- HypHC/set_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Récupère le chemin absolu du dossier contenant ce script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Active l'environnement conda global
-source ~/miniconda3/bin/activate hyphc_env
-
-
